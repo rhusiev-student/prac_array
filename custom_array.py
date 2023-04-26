261ba1262a5363613a053e4ad8adad5a202a0228
radomyr <h.radomyr@proton.me>
2023-04-26 12:35:05 +0300

    feat: implement Array and Array2D

--- custom_array.py
+++ custom_array.py
@@ -16,9 +16,6 @@ class Array:
     def __len__(self) -> int:
         return self._size
 
-    def length(self) -> int:
-        return self._size
-
     def nullify(self) -> None:
         self.clear(None)
 
@@ -29,20 +26,38 @@ class Array:
     def __getitem__(self, index: int) -> object:
         return self._array[index]
 
-    def getitem(self, index: int) -> object:
-        return self._array[index]
-
     def __setitem__(self, index: int, value: object) -> None:
         self._array[index] = value
 
-    def setitem(self, index: int, value: object) -> None:
-        self._array[index] = value
-
-    def iterator(self) -> iter:
+    def __iter__(self):
         return (self._array[i] for i in range(self._size))
-
-    def __iter__(self) -> iter:
-        return self.iterator()
 
     def __str__(self) -> str:
         return str([self._array[i] for i in range(self._size)])
+
+    def __repr__(self) -> str:
+        return str(self)
+
+
+class Array2D(Array):
+    """A custom 2D array class."""
+
+    def __init__(self, shape: tuple) -> None:
+        if not isinstance(shape, tuple) or len(shape) != 2:
+            raise ValueError("Invalid shape")
+        self._shape = shape
+        super().__init__(shape[0])
+        for i in range(shape[0]):
+            self._array[i] = Array(shape[1])
+
+    def __getitem__(self, index: tuple) -> object:
+        return self._array[index[0]][index[1]]
+
+    def __setitem__(self, index: tuple, value: object) -> None:
+        self._array[index[0]][index[1]] = value
+
+    def __iter__(self):
+        return (self._array[i] for i in range(self._shape[0]))
+
+    def __str__(self) -> str:
+        return str([self._array[i] for i in range(self._shape[0])])
